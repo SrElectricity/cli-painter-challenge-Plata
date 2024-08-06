e6a668bc4a7c63df39e517a4253111d65992ec51
SrElectricity <nicolasdrv10@gmail.com>
2024-08-06 11:40:43 -0500

    Terminado las clases y atributos del triangulo

--- painter/model.py
+++ painter/model.py
@@ -34,7 +34,26 @@ class Triangle:
         self.point_3: Point = point_3
 
     def area(self):
-        return ((self.point_1.x-self.point_2.y)+(self.point_2.x+self.point_3.y)+(self.point_3.x+self.point_1.y))
+        return self.point_1.x*self.point_2.y+self.point_2.x*self.point_3.y+self.point_3.x*self.point_1.y-self.point_1.x*self.point_3.y-self.point_3.x*self.point_2.y-self.point_2.x*self.point_1.y
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+        return f"Triangle with vertices at ({self.point_1.x}, {self.point_1.y}, {self.point_2.x}, {self.point_2.y} and {self.point_3.x}, {self.point_3.y}"
+
+class Rectangle:
+    def __init__(self, point_1: Point, point_2: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+
+
+
+
 
 
 
